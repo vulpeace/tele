5aed661d5b6c29f4a2cae3404aca30394cfe5761
vulpeace <35395586+vulpeace@users.noreply.github.com>
2026-09-01 02:52:57 +0500

    environment: update configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 import js from '@eslint/js';
-// import tseslint from 'typescript-eslint';
+import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default tseslint.config(
   js.configs.recommended,
-  // ...tseslint.configs.recommended,
+  ...tseslint.configs.recommended,
 );
