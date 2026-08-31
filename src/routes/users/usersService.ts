53a30bdaa576dc8942e18520f7beea4c63f570f7
vulpeace <35395586+vulpeace@users.noreply.github.com>
2026-09-01 02:42:56 +0500

    UsersService: improve UUID validation

--- src/routes/users/usersService.ts
+++ src/routes/users/usersService.ts
@@ -1,9 +1,9 @@
 import type { NewUser, User, UserDiff } from "@/src/interfaces/user.js";
-import { validate } from "uuid";
 import { hash } from "bcryptjs";
 import { createUser, deleteUser, getUserListeners, getUsers, updateUser } from "@/src/db/users/index.js";
 import { addListenersToConfig } from "@/src/configConstructor/mihomoConfig.js";
 import { getListeners } from "@/src/db/listeners/index.js";
+import { ValidateError } from "tsoa";
 
 export class UsersService {
   public get(username?: string): User[] {
@@ -12,10 +12,13 @@ export class UsersService {
   }
 
   public async create(user: NewUser): Promise<string> {
+    const uuidv4Regexp = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
     const uuid = user.uuid;
-    if (uuid && !validate(uuid))
+    if (uuid && !uuidv4Regexp.test(uuid))
     {
-      throw new Error(`Invalid UUIDv4 for ${user.name}`);
+      throw new ValidateError({ uuid: {
+        message: "Invalid UUIDv4"
+      }}, "Validation Failed");
     }
     const path = btoa(await hash(user.name + Date.now(), 10));
     createUser({ ...user, path });
