76d7a91afef65e2fa0104bf8f37cdecdc9d89286
vulpeace <35395586+vulpeace@users.noreply.github.com>
2026-09-03 00:35:44 +0500

    cleanup: remove redundant routes

--- src/routes/sub/subService.ts
+++
@@ -1,8 +0,0 @@
-// import { stringify } from "yaml";
-// import { constructClientConfig } from "@/src/configConstructor/clientConfig.js";
-
-// export class SubscriptionService {
-//   public get(path: string): string {
-//     return stringify(constructClientConfig(path));
-//   }
-// }
